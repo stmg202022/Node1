d1b8b26a6e716bc51f0c21b006d08c677c2d8a91
Samson Tmg <stmg202023@gmail.com>
2023-03-14 08:01:00 +0545

    req.url startWith, split, and modification of index with dynamically

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const http = require("http");
 const fs = require("fs");
 
 const index = fs.readFileSync("index.html", "utf-8"); //static html
-const data = fs.readFileSync("data.json", "utf-8");
+// const data = fs.readFileSync("data.json", "utf-8");
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const products = data.products;
 // const data2 = {
 //   name: "samson",
 // };
-
 //================Static hosting =============================
 // console.log(index);
 
@@ -28,6 +29,30 @@ const data = fs.readFileSync("data.json", "utf-8");
 //================================================================
 
 const server = http.createServer((req, res) => {
+  if (req.url.startsWith("/products")) {
+    const id = req.url.split("/")[2];
+    // console.log(id);
+    const item = products.find((p) => p.id === +id);
+    // console.log(item.title);
+
+    res.setHeader("Content-Type", "Text/html");
+
+    let modifiedIndex = index
+      .replace("Hello World", item.title)
+      .replace("**url**", item.thumbnail);
+
+    res.end(modifiedIndex);
+
+    return;
+  }
+  // case "/products":
+  //   res.setHeader("Content-Type", "text/html");
+  //   let modifiedIndex = index
+  //     .replace("Hello World", products[0].title)
+  //     .replace("**url**", products[0].thumbnail);
+
+  //   res.end(modifiedIndex);
+
   console.log(req.url);
 
   switch (req.url) {
@@ -37,8 +62,9 @@ const server = http.createServer((req, res) => {
       break;
     case "/api":
       res.setHeader("Content-Type", "application/json");
-      res.end(data);
+      res.end(JSON.stringify(data));
       break;
+
     default:
       res.writeHead(404);
       res.end();
